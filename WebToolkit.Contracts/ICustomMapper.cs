fb15f29c22f856ea0faa9de5752b5a3c283d7a75
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-06-10 21:48:17 +0100

    Added ICustomMapper

---
+++ WebToolkit.Contracts/ICustomMapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebToolkit.Contracts
+{
+    public interface ICustomMapper<TSource, TDestination, T>
+    {
+        ISwitch<T, Func<TSource, TDestination>> CustomMapperSwitch { get; }
+        TDestination Map(T type, TSource source);
+    }
+}
