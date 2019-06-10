0e694c7d6bec88b31ad156c7282c1597669c149e
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-06-10 23:35:51 +0100

    Added lock

---
+++ WebToolkit.Contracts/ILock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebToolkit.Contracts
+{
+    public interface ILock<TResult>
+    {
+        Func<TResult, TResult> DoWork { get; }
+        TResult Value { get; }
+
+        void Run();
+    }
+}
