ce77757bbf73e402214e20ae0a8833dc0002ad8e
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-07-05 17:19:08 +0100

    Added ServiceCollection extension Added IServiceRegistration

---
+++ WebToolkit.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebToolkit.Common.Providers;
+using WebToolkit.Contracts.Data;
+using WebToolkit.Contracts.Providers;
+
+namespace WebToolkit.Common.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterRepositories<TDbContext>(this IServiceCollection services, params Type[] entities) where TDbContext : DbContext
+        {
+            foreach (var entity in entities)
+            {
+                var repositoryDefinitionType = typeof(IRepository<>);
+                var repositoryType = typeof(DefaultRepository<,>);
+
+                var genericRepositoryDefinitionType = repositoryDefinitionType.MakeGenericType(entity);
+                var genericRepositoryType = repositoryType.MakeGenericType(typeof(TDbContext), entity);
+
+                services.AddScoped(genericRepositoryDefinitionType, genericRepositoryType);
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddDefaultValueProvider<TModel>(this IServiceCollection services, Action<TModel> defaults)
+        {
+            return services.AddSingleton<IDefaultValueProvider<TModel>>(new DefaultValuesProvider<TModel>(defaults));
+        }
+
+        
+        public static IServiceCollection AddDefaultValueProvider<TModel>(this IServiceCollection services)
+        {
+            return services.AddSingleton<IDefaultValueProvider<TModel>>(new DefaultValuesProvider<TModel>(model => {}));
+        }
+    }
+}
