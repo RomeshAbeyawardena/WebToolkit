fe200eaa4bd5d5dbe9f325d1b0e8835fe06bc05c
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-07-06 17:52:50 +0100

    Added JSonSettings

---
+++ WebToolkit.Common/DefaultJsonSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebToolkit.Contracts;
+
+namespace WebToolkit.Common
+{
+    public class DefaultJSonSettings : IJSonSettings
+    {
+        public DefaultJSonSettings(JsonLoadSettings jsonLoadSettings, JsonSerializer jsonSerializer)
+        {
+
+        }
+
+        public DefaultJSonSettings(Action<IJSonSettings> jsonSettings)
+        {
+            jsonSettings(this);
+        }
+
+        public JsonLoadSettings LoadSettings { get; set; }
+        public JsonSerializer Serializer { get; set; }
+    }
+}
