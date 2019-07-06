4869cb140f3cdf4926b1355705c27632a300fdce
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-07-06 23:04:34 +0100

    Added GetRequiredService Added RegisterServicesFromAssemblies

--- WebToolkit.Common/Extensions/ServiceCollectionExtensions.cs
+++ WebToolkit.Common/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebToolkit.Common.Providers;
@@ -21,6 +24,30 @@ namespace WebToolkit.Common.Extensions
                 var genericRepositoryType = repositoryType.MakeGenericType(typeof(TDbContext), entity);
 
                 services.AddScoped(genericRepositoryDefinitionType, genericRepositoryType);
+            }
+
+            return services;
+        }
+
+        public static TService GetRequiredService<TService>(this IServiceCollection services)
+        {
+            return services.BuildServiceProvider()
+                .GetRequiredService<TService>();
+        }
+
+        public static IServiceCollection RegisterServicesFromAssemblies<TServiceRegistration, TServiceBroker>(
+            this IServiceCollection services, Func<TServiceBroker, IEnumerable<Assembly>> getAssemblies,
+            Action<TServiceRegistration, IServiceCollection> registerServices) 
+            where TServiceRegistration : class
+        {
+            foreach (var a in getAssemblies(Activator.CreateInstance<TServiceBroker>()))
+            {
+                foreach (var t in a.GetTypes().Where(type => type.IsClass 
+                                                             && type.GetInterface(nameof(IServiceRegistration)) == typeof(TServiceRegistration)))
+                {
+                    var serviceRegistration = Activator.CreateInstance(t) as TServiceRegistration;
+                    registerServices(serviceRegistration, services);
+                }
             }
 
             return services;
