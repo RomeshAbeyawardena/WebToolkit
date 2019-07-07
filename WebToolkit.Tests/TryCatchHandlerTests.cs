6d2aefd0cd4528d54bfa2093e01bc3e221727019
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-07-07 20:55:30 +0100

    AddedTryCatchHandler

---
+++ WebToolkit.Tests/TryCatchHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebToolkit.Common;
+using Xunit;
+
+namespace WebToolkit.Tests
+{
+    public class TryCatchHandlerTests
+    {
+        [Fact]
+        public void TryCatchHandler_Handle()
+        {
+            Assert.Equal(default, TryCatchHandler.Handle<bool>(() => throw new NotSupportedException(),
+                ex => Assert.Equal(typeof(NotSupportedException), ex.GetType()), catchExceptionTypesArray: typeof(NotSupportedException)));
+        }
+
+        [Fact]
+        public void TryCatchHandler_HandleAsync()
+        {
+            Assert.Equal(default, TryCatchHandler.HandleAsync<bool>(() => throw new NotSupportedException(),
+                ex =>
+                {
+                    Assert.Equal(typeof(NotSupportedException), ex.GetType());
+                    return Task.FromResult(true);
+                }, catchExceptionTypesArray: typeof(NotSupportedException)).Result);
+        }
+    }
+}
