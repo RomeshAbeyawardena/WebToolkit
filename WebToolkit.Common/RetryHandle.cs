b74b29cfe0658acd5a7f1d9d62a372922da631bf
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2019-07-08 15:00:15 +0100

    Added RetryHandle

---
+++ WebToolkit.Common/RetryHandle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace WebToolkit.Common
+{
+    public class RetryHandle
+    {
+        private RetryHandle(Action<RetryHandleOptions> retryOptionsAction, params Type[] exceptionTypes)
+        {
+            Options = new RetryHandleOptions();
+            Options.ExceptionTypes = exceptionTypes;
+            retryOptionsAction(Options);
+        }
+        private int attempts;
+
+        public RetryHandleOptions Options { get; }
+
+        public T Handle<T>(Func<T> tryFunc, Action<Exception> catchFunc = null, Action finallyFunc = null)
+        {
+            T result = default;
+
+            var success = false;
+            while (!success && attempts < Options.MaximumAttempts)
+                result = TryCatchHandler.Handle(() =>
+                {
+                    var value = tryFunc();
+                    success = true;
+                    return value;
+                } , ex =>
+                {
+                    catchFunc?.Invoke(ex);
+                    Thread.Sleep(Options.Timeout);
+                    attempts++;
+                }, finallyFunc, Options.ExceptionTypes.ToArray());
+
+            return result;
+        }
+
+        public static RetryHandle Create(Action<RetryHandleOptions> retryOptionsAction, params Type[] exceptionTypes)
+        {
+            return new RetryHandle(retryOptionsAction, exceptionTypes);
+        }
+    }
+}
---
+++ WebToolkit.Tests/RetryHandleTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using WebToolkit.Common;
+using Xunit;
+
+namespace WebToolkit.Tests
+{
+    public class RetryHandleTests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(10)]
+        public void RetryHandle_Retries_x_times(int times)
+        {
+            var retryAttempt = 0;
+            var sut = RetryHandle.Create(options =>
+            {
+                options.MaximumAttempts = times;
+                options.Timeout = 1000;
+            } ,typeof(TimeoutException));
+
+            sut.Handle<string>(() => throw new TimeoutException(), ex => { retryAttempt++; });
+            Assert.Equal(times, retryAttempt);
+        }
+
+        [Theory]
+        [InlineData(1, "Hello")]
+        [InlineData(3, "John Doe")]
+        [InlineData(5, "Jane Doe")]
+        [InlineData(10, "Yet, Another test")]
+        public void RetryHandle_Retries_x_times_then_passes(int times, string passValue)
+        {
+            var retryAttempt = 0;
+            var sut = RetryHandle.Create(options =>
+            {
+                options.MaximumAttempts = times;
+                options.Timeout = 1000;
+            } ,typeof(TimeoutException));
+
+            Assert.Equal(passValue,
+            sut.Handle<string>(() =>
+            {
+                if (retryAttempt == times - 1)
+                    return passValue;
+                throw new TimeoutException();
+            }, ex => { retryAttempt++; }));
+
+            Assert.Equal(times - 1, retryAttempt);
+        }
+    }
+}
