c3161b47570bfaa1075e9069368e029bd5a82b7f
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2019-07-08 13:08:06 +0100

    Added ILock non-generic

--- WebToolkit.Common/Lock.cs
+++ WebToolkit.Common/Lock.cs
@@ -4,23 +4,50 @@ using WebToolkit.Contracts;
 
 namespace WebToolkit.Common
 {
+    public sealed class Lock : ILock
+    {
+        public Action DoWork { get; }
+        public void Run()
+        {
+            lock(_lockObject)
+                DoWork();
+        }
+
+        public static Lock Create(Action doWork)
+        {
+            return new Lock(doWork);
+        }
+
+        public static Lock<TResult> Create<TResult>(TResult initialValue, Func<TResult, TResult> doWork)
+        {
+            return Lock<TResult>.Create(initialValue, doWork);
+        }
+
+        private readonly object _lockObject = new object();
+        private Lock(Action doWork)
+        {
+            DoWork = doWork;
+        }
+    }
+
     public sealed class Lock<TResult> : ILock<TResult>
     {
         private readonly object _lockObject = new object();
-        private readonly Random _random = new Random();
         public static Lock<TResult> Create(TResult initialValue, Func<TResult, TResult> doWork)
         {
             return new Lock<TResult>(initialValue, doWork);
         }
 
         public TResult Value { get; private set; }
+
+        Action ILock.DoWork => throw new NotSupportedException();
+
         public Func<TResult, TResult> DoWork { get; }
         public void Run()
         {
             lock (_lockObject)
             {
                 Value = DoWork(Value);
-                Thread.Sleep(_random.Next(100, 1000));
             }
         }
 
