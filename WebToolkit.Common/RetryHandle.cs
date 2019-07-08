71607e27ff7e08cb51a39d0a82983fa570dc5ddf
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2019-07-08 15:33:31 +0100

    Added HandleAsync

--- WebToolkit.Common/RetryHandle.cs
+++ WebToolkit.Common/RetryHandle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebToolkit.Common
 {
@@ -13,6 +14,29 @@ namespace WebToolkit.Common
         }
 
         public RetryHandleOptions Options { get; }
+
+        public async Task<T> HandleAsync<T>(Func<Task<T>> tryFunc, Action<Exception> catchFunc = null, Func<Task> finallyFunc = null)
+        {
+            T result = default;
+            var attempts = 0;
+
+            var success = false;
+            while (!success && attempts < Options.MaximumAttempts)
+                result = await TryCatchHandler.HandleAsync(async() =>
+                {
+                    var value = await tryFunc();
+                    success = true;
+                    return value;
+                } , async(ex) =>
+                {
+                    await Task.CompletedTask;
+                    catchFunc?.Invoke(ex);
+                    Thread.Sleep(Options.Timeout);
+                    attempts++;
+                }, finallyFunc, catchExceptionTypesArray: Options.ExceptionTypes.ToArray());
+
+            return result;
+        }
 
         public T Handle<T>(Func<T> tryFunc, Action<Exception> catchFunc = null, Action finallyFunc = null)
         {
