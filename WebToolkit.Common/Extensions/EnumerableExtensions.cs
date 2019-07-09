70304b6a5b7921b91947ccc862d327312e8ea70c
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2019-07-09 11:57:03 +0100

    Added enumerable extensions

---
+++ WebToolkit.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebToolkit.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action, Func<T, bool> whereExpression = null)
+        {
+            foreach (var item in whereExpression == null 
+                ? items 
+                : items.Where(whereExpression))
+            {
+                action(item);
+            }
+        }
+    }
+}
---
+++ WebToolkit.Tests/ForEachTests.cs
@@ -0,0 +1,28 @@
+﻿using WebToolkit.Common.Extensions;
+using Xunit;
+
+namespace WebToolkit.Tests
+{
+    public class ForEachTests
+    {
+        [Fact]
+        public void ForEach_affects_all_elements()
+        {
+            var total = 0;
+            var items = new[] {1, 4, 6, 8, 12};
+            items.ForEach(item => total += item);
+
+            Assert.Equal(31, total);
+        }
+
+        [Fact]
+        public void ForEach_affects_filtered_elements()
+        {
+            var total = 0;
+            var items = new[] {1, 4, 6, 8, 12};
+            items.ForEach(item => total += item, item => item < 12);
+
+            Assert.Equal(19, total);
+        }
+    }
+}
