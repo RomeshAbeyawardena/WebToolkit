99cb8076de5a9075541bfba91b6fdabbd9353d82
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2019-07-19 14:07:25 +0100

    Added AssemblyExtensions

---
+++ WebToolkit.Common/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace WebToolkit.Common.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static Assembly GetAssembly<T>(this Assembly assembly)
+        {
+            return Assembly.GetAssembly(typeof(T));
+        }
+    }
+}
