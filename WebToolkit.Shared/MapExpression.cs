f9ad937fd88e5394d4ac8e4397d6556cf08c8bcc
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-08-05 00:02:11 +0100

    Implemented MapExpression

---
+++ WebToolkit.Shared/MapExpression.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using AutoMapper;
+
+namespace WebToolkit.Shared
+{
+    public static class MapExpression
+    {
+        public static TDestinationContainer Convert<TSourceContainer, TDestinationContainer>(TSourceContainer container, 
+            Func<IMapper> mapper)
+        {
+            var sourceContainerType = typeof(TSourceContainer);
+            
+            var destinationContainerType = typeof(TDestinationContainer);
+            var sourceType = sourceContainerType.GenericTypeArguments[0];
+            var destinationType = destinationContainerType.GenericTypeArguments[0];
+
+            var sourceProps = sourceContainerType.GetProperties(); 
+            var sourceProp = sourceProps.FirstOrDefault(p => p.PropertyType == sourceType);
+            
+            if(sourceProp == null)
+                throw new NullReferenceException("Source property not found");
+
+            var mappedValue = mapper().Map(sourceProp.GetValue(container), sourceType, destinationType);
+
+            var destinationContainer =  (TDestinationContainer)Activator.CreateInstance(destinationContainerType);
+
+            var destinationProp = destinationContainerType.GetProperty(sourceProp.Name);
+
+            if(destinationProp == null)
+                throw new NullReferenceException("Source property not found");
+
+            destinationProp.SetValue(destinationContainer, mappedValue);
+
+            return destinationContainer;
+        }
+    }
+}
---
+++ WebToolkit.Tests/MapExpressionTest.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Moq;
+using WebToolkit.Shared;
+using Xunit;
+
+namespace WebToolkit.Tests
+{
+    public class MapExpressionTest
+    {
+        [Fact]
+        public void Convert()
+        {
+            var expected = new B
+            {
+                Context = int.MinValue,
+                Name = "Mapped",
+                Value = int.MaxValue
+            };
+
+            var actual = new A
+            {
+                Context = int.MinValue,
+                Name = "Mapped",
+                Value = int.MaxValue
+            };
+
+            var mapperMock = new Mock<IMapper>();
+                mapperMock
+                    .Setup(a => a.Map(It.IsAny<A>(), typeof(A), typeof(B)))
+                    .Returns(expected)
+                    .Verifiable();
+            var result =  MapExpression.Convert<MyContainer<A>, MyContainer<B>>(new MyContainer<A>
+            {
+                Result = actual
+            }, () => mapperMock.Object);
+            mapperMock.Verify(a => a.Map(It.IsAny<A>(),typeof(A), typeof(B)), Times.Once);
+            
+            Assert.Same(expected, result.Result);
+        }
+
+        internal class A
+        {
+            public int Value { get; set; }
+            public string Name { get; set; }
+            public object Context { get; set; }
+        }
+
+        internal class B
+        {
+            public int Value { get; set; }
+            public string Name { get; set; }
+            public object Context { get; set; }
+        }
+
+        internal class MyContainer<TModel>
+        {
+            public TModel Result { get; set; }
+        }
+    }
+}
