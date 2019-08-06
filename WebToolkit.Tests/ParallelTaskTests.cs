c26291757a4e35942a8cc9cf310ad8cbd1e548d1
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2019-08-06 15:03:06 +0100

    Added ParallellTask and tests

---
+++ WebToolkit.Tests/ParallelTaskTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebToolkit.Common;
+using Xunit;
+
+namespace WebToolkit.Tests
+{
+    public class RaceTests
+    {
+        async Task<decimal> SlowProcess(int delay)
+        {
+            await Task.Delay(delay);
+            return 224959.42m;
+        }
+
+        [Fact]
+        public async Task Race_Start()
+        {
+            var racer = new ParallelTask();
+            var result = await racer.Start(SlowProcess(5000), async(slowProcess) =>
+            {
+                var amount = await SlowProcess(5500);
+                amount += await slowProcess;
+
+                return amount;
+            } );
+
+            Assert.Equal(449918.84m, result);
+        }
+
+        [Fact]
+        public async Task Race_StartMany()
+        {
+            var racer = new ParallelTask();
+            var result = await racer
+                .StartMany(async (tasks) =>
+                {
+                    await Task.Delay(5500);
+                    var res = await Task.WhenAll(tasks.ToArray());
+                    return res.Sum();
+                }, SlowProcess(1000), SlowProcess(2000), SlowProcess(3000), SlowProcess(4000), SlowProcess(5000));
+
+            Assert.Equal(1124797.10m, result);
+        }
+    }
+}
