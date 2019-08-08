cb4c7b1a33125d685e6fd68e8a3473ab9f3f7848
Anthony Abeyawardena <Anthony.Abeyawardena@connellsgroup.co.uk>
2019-08-08 15:43:01 +0100

    Added HtmlMinify

--- WebToolkit.Common/Extensions/StringExtensions.cs
+++ WebToolkit.Common/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace WebToolkit.Common.Extensions
@@ -8,6 +9,11 @@ namespace WebToolkit.Common.Extensions
         public static IEnumerable<byte> ToByteEnumerable(this string value, Encoding encoding)
         {
             return encoding.GetBytes(value);
+        }
+
+        public static string ReplaceAll(this string value, string replacement, params string[] replaceValues)
+        {
+            return replaceValues.Aggregate(value, (current, replaceValue) => current.Replace(replaceValue, replacement));
         }
     }
 }
---
+++ WebToolkit.Common/HtmlMinify.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using HtmlAgilityPack;
+using WebToolkit.Common.Extensions;
+
+namespace WebToolkit.Common
+{
+    public static class HtmlMinify
+    {
+        public static readonly string[] HtmlMinifyReplacementValues = {Environment.NewLine, "\r", "\n", "\t"};
+        public static string Minify(string value)
+        {
+            
+            var document = new HtmlDocument();
+            document.LoadHtml(value);
+            
+            var htmlStringBuilder = new StringBuilder(document.DocumentNode.OuterHtml);
+            htmlStringBuilder.Append(GetChildNodeHtml(document.DocumentNode.ChildNodes));
+                    
+            return htmlStringBuilder.ToString();
+        }
+
+        private static string GetChildNodeHtml(HtmlNodeCollection childNodes)
+        {
+            var htmlStringBuilder = new StringBuilder();
+
+            foreach (var documentNodeChildNode in childNodes)
+            {
+                var replacedValue =
+                    documentNodeChildNode.InnerHtml
+                        .ReplaceAll(string.Empty, HtmlMinifyReplacementValues)
+                        .Trim();
+                htmlStringBuilder.Append(replacedValue);
+
+                if (documentNodeChildNode.HasChildNodes)
+                    GetChildNodeHtml(documentNodeChildNode.ChildNodes);
+            }
+
+            return htmlStringBuilder.ToString();
+        }
+    }
+}
