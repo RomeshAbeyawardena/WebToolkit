97550b42d56d140db7c72494ec3c2446c64197d0
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-08-09 23:33:03 +0100

    Add FilterExpressionParameter

--- WebToolkit.Common/FilterExpressionBuilder.cs
+++ WebToolkit.Common/FilterExpressionBuilder.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using WebToolkit.Shared;
 
 namespace WebToolkit.Common
 {
     public sealed class FilterExpressionBuilder
     {
         public static FilterExpressionBuilder<TModel> CreateBuilder<TModel>(
-            IDictionary<string, object> filterDictionary)
+            IDictionary<string, FilterExpressionParameter> filterDictionary)
         {
             return FilterExpressionBuilder<TModel>.CreateBuilder(filterDictionary);
         }
@@ -16,32 +18,81 @@ namespace WebToolkit.Common
 
     public class FilterExpressionBuilder<TModel>
     {
-        private readonly IDictionary<string, object> _filterDictionary;
+        private readonly IDictionary<string, FilterExpressionParameter> _filterDictionary;
 
-        private FilterExpressionBuilder(IDictionary<string, object> filterDictionary)
+        private FilterExpressionBuilder(IDictionary<string, FilterExpressionParameter> filterDictionary)
         {
             _filterDictionary = filterDictionary;
         }
 
-        private Expression<Func<TModel, bool>> GetFilterExpression (IDictionary<string, object> filterDictionary)
+        private static Expression GetUnaryExpressionByOperator(Operator operatorValue, Expression leftExpression, Expression rightExpression)
+        {
+            switch (operatorValue)
+            {
+                case Operator.And:
+                    return Expression.And(leftExpression, rightExpression ?? throw new ArgumentNullException(nameof(rightExpression)));
+                case Operator.Not:
+                    return Expression.And(leftExpression, Expression.Not(rightExpression));
+                case Operator.Or:
+                    return Expression.Or(leftExpression, rightExpression  
+                                                         ?? throw new ArgumentNullException(nameof(rightExpression)));
+                default:
+                    throw new FormatException();
+            }
+        }
+
+        private static Expression GetBinaryExpressionByComparisonType(ComparisonType comparisonType, Expression leftExpression, Expression rightExpression)
+        {
+            if (comparisonType.HasFlag(ComparisonType.Equal | ComparisonType.GreaterThan))
+                return Expression.GreaterThanOrEqual(leftExpression, rightExpression);
+
+            if (comparisonType.HasFlag(ComparisonType.Equal | ComparisonType.LessThan))
+                return Expression.LessThanOrEqual(leftExpression, rightExpression);
+
+            switch (comparisonType)
+            {
+                case ComparisonType.Equal:
+                    return Expression.Equal(leftExpression, rightExpression ?? throw new ArgumentNullException(nameof(rightExpression)));
+                case ComparisonType.GreaterThan:
+                    return Expression.GreaterThan(leftExpression, rightExpression ?? throw new ArgumentNullException(nameof(rightExpression)));
+                case ComparisonType.LessThan:
+                    return Expression.LessThan(leftExpression, rightExpression ?? throw new ArgumentNullException(nameof(rightExpression)));
+                case ComparisonType.Contains:
+                    return Expression.Call(leftExpression, "Contains", null, rightExpression);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private Expression<Func<TModel, bool>> GetFilterExpression (IDictionary<string, FilterExpressionParameter> filterDictionary)
         {
             var modelType = typeof(TModel);
             var objectType = typeof(object);
             var expressionParameter = Expression.Parameter(modelType, "model");
+            Expression assignedExpression = null;
+            foreach (var filter in filterDictionary)
+            {
+                var filterExpressionParameter = filter.Value;
+                var propertyExpression = Expression.PropertyOrField(expressionParameter, filter.Key);
+                var constantExpression = Expression.Constant(filterExpressionParameter.Value, filterExpressionParameter.ValueType);
+                var equalityExpression = GetBinaryExpressionByComparisonType(filterExpressionParameter.ComparisonType, 
+                    propertyExpression, constantExpression);
 
-            var assignedExpression = (from o in filterDictionary
-                let body = Expression.PropertyOrField(expressionParameter, o.Key)
-                let constant = Expression.Constant(o.Value)
-                select Expression.Equal(body, constant))
-                    .Aggregate<BinaryExpression, Expression>(null, (current, equateExpression) => current == null
-                ? equateExpression
-                : Expression.And(current, equateExpression));
+                if (assignedExpression == null)
+                {
+                    assignedExpression = equalityExpression;
+                    continue;
+                }
+
+                assignedExpression = GetUnaryExpressionByOperator(filterExpressionParameter.Operator,
+                    assignedExpression, equalityExpression);
+            }
 
             var expression = Expression.Lambda<Func<TModel, bool>>(assignedExpression, expressionParameter);
             return expression;
         }
 
-        public static FilterExpressionBuilder<TModel> CreateBuilder(IDictionary<string, object> filterDictionary)
+        public static FilterExpressionBuilder<TModel> CreateBuilder(IDictionary<string, FilterExpressionParameter> filterDictionary)
         {
             return new FilterExpressionBuilder<TModel>(filterDictionary);
         }
--- WebToolkit.Tests/FilterExpressionBuilderTest.cs
+++ WebToolkit.Tests/FilterExpressionBuilderTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
 using WebToolkit.Common;
+using WebToolkit.Shared;
 using Xunit;
 
 namespace WebToolkit.Tests
@@ -13,13 +14,26 @@ namespace WebToolkit.Tests
         public void GetFilterExpression()
         {
             var a = FilterExpressionBuilder.CreateBuilder<IncludeExpressionTests.TestClass>(
-                new Dictionary<string, object>(new Dictionary<string, object>
+                new Dictionary<string, FilterExpressionParameter>
                 {
-                    {"a","a"},
-                    { "b", "b" },
-                    { "c", 12345 }
-                })).FilterExpression;
-            
+                    {"b", FilterExpressionParameter.Create(f =>
+                    {
+                        f.Operator = Operator.And;
+                        f.Value = "Test";
+                        f.ComparisonType = ComparisonType.Equal;
+                    })},
+                    { "a", FilterExpressionParameter.Create(f => {
+                        f.Operator = Operator.Not;
+                        f.Value = "Test21321";
+                        f.ComparisonType = ComparisonType.Contains;
+                    })},
+                    { "c", FilterExpressionParameter.Create(f => {
+                        f.Operator = Operator.Or;
+                        f.Value = 64571;
+                        f.ComparisonType = ComparisonType.LessThan;
+                    })}
+                    }).FilterExpression;
+
             Assert.IsAssignableFrom<Expression<Func<IncludeExpressionTests.TestClass, bool>>>(a);
         }
     }
