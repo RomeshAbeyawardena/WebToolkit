335abeefaa36bb41d7d5a16ff08e697f82ed82ed
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-08-15 23:48:03 +0100

    Added PagerFactory Added CacheDictionary

---
+++ WebToolkit.Common/Factories/PagerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using WebToolkit.Contracts;
+using WebToolkit.Contracts.Factories;
+
+namespace WebToolkit.Common.Factories
+{
+    public class PagerFactory : IPagerFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public IPager<TModel> GetPager<TModel>()
+        {
+            return _serviceProvider.GetRequiredService<IPager<TModel>>();
+        }
+
+        public PagerFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+}
