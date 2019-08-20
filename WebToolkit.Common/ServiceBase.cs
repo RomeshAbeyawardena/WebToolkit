5b075933f1c406723a08d7f324d7167da34df47d
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-08-20 19:48:21 +0100

    Added additional helper classes

---
+++ WebToolkit.Common/ServiceBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebToolkit.Contracts;
+using WebToolkit.Contracts.Data;
+
+namespace WebToolkit.Common
+{
+    public class ServiceBase : IService
+    {
+        public T RetrieveAndAttach<TRepository, T>(TRepository repository, Func<T> retrievalCallback, bool attach) where TRepository : IRepository<T> where T : class
+        {
+            var retrievedEntry = retrievalCallback();
+            if(retrievedEntry == null)
+                return default;
+
+            return attach 
+                ? repository.Attach(retrievedEntry) 
+                : retrievedEntry;
+        }
+
+        public async Task<T> RetrieveAndAttach<TRepository, T>(TRepository repository, Func<Task<T>> retrievalCallback, bool attach) where TRepository : IRepository<T> where T : class
+        {
+            var retrievedEntry = await retrievalCallback();
+            if(retrievedEntry == null)
+                return default;
+
+            return attach 
+                ? repository.Attach(retrievedEntry) 
+                : retrievedEntry;
+        }
+    }
+}
