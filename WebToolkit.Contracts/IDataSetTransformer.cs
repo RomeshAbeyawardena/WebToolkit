3895065c9fd5f2f5b3a4d9dc42d392a00696845a
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-08-23 20:56:53 +0100

    Added additional methods

---
+++ WebToolkit.Contracts/IDataSetTransformer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace WebToolkit.Contracts
+{
+    public interface IDataSetTransformer
+    {
+        IEnumerable<TDestination> TransformDataSet<TDestination>(DataSet dataSet, string columnOrder, bool includesHeaders = false);
+    }
+}
---
+++ WebToolkit.Contracts/IExcelDocumentParser.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace WebToolkit.Contracts
+{
+    public interface IExcelDocumentParser
+    {
+        DataSet ParseFile(string filename);
+    }
+}
