1694b8db50fca391dc1512830b5da3d8fa145622
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-08-28 22:28:15 +0100

    Added AsyncMessage Added AsyncMessageQueue

---
+++ WebToolkit.Shared/AsyncMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebToolkit.Shared
+{
+    public class AsyncMessage
+    {
+        public AsyncMessage(object arguments, Func<object, Task<object>> request, Action<object> onCompletion)
+        {
+            RequestArguments = arguments;
+            Request = request;
+            OnCompletion = onCompletion;
+        }
+
+        public Action<object> OnCompletion { get; }
+        public Func<object, Task<object>> Request { get; }
+        public object RequestArguments { get; }
+    }
+}
---
+++ WebToolkit.Tests/AsyncMessageQueueTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WebToolkit.Common;
+using WebToolkit.Shared;
+using Xunit;
+
+namespace WebToolkit.Tests
+{
+    public class AsyncMessageQueueTests
+    {
+        [Fact]
+        public void BeginProcessingQueue()
+        {
+            using (var sut = AsyncMessageQueue.CreateMessageQueue(500))
+            {
+                sut.BeginProcessingQueue();
+                sut.Enqueue(new AsyncMessage(12345, Request, 
+                    OnCompletion));
+                Thread.Sleep(3000);
+            } 
+        }
+
+        private async Task<object> Request(object args)
+        {
+            return await Task.FromResult(args);
+        }
+        
+        private void OnCompletion(object result)
+        {
+            Assert.Equal(512345, result);
+        }
+    }
+}
