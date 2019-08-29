231e8ae6cc42a743f6f8a25713c8866aeff0da7c
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2019-08-29 18:20:11 +0100

    Added ResourceManagerProvider

---
+++ WebToolkit.Contracts/IResourceManagerProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Resources;
+
+namespace WebToolkit.Contracts
+{
+    public interface IResourceManagerProvider
+    {
+        ResourceManager ResourceManager { get; }
+        T Bind<T>(CultureInfo cultureInfo = null, 
+            IDictionary<string, object> mockResourceDictionary = null);
+    }
+}
